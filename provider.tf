556e95a0c77e38031b48a6c30e6c9193782d283b
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-04-12 13:47:44 +0530

    Update roboshop-jenkins/

--- provider.tf
+++ provider.tf
@@ -1,6 +1,14 @@
 provider "jenkins" {
   server_url = "https://172.31.1.223:8080"
-  username   = "admin"
-  password   = "admin123"
+  username   = data.aws_ssm_parameter.jenkins_user.value
+  password   = data.aws_ssm_parameter.jenkins_pass.value
+}
+
+data "aws_ssm_parameter" "jenkins_user" {
+  name = "jenkins.user"
+}
+
+data "aws_ssm_parameter" "jenkins_pass" {
+  name = "jenkins.pass"
 }
 
