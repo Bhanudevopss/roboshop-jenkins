001226fd17a44c1757f4f8f8e18d9b34d8547fbc
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-04-13 10:18:52 +0530

    Update roboshop-jenkins/

--- vars.tf
+++ vars.tf
@@ -5,13 +5,13 @@ variable "folders" {
 variable "jobs" {
   default = [
     { name = "roboshop", folder = "Infra", repo_url = "https://github.com/Bhanudevopss/roboshop-infra" },
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/roboshop-infra" },
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/roboshop-infra" },
-    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/roboshop-infra" },
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/roboshop-infra" },
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/roboshop-infra" },
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/roboshop-infra" },
-    { name = "dispatch", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/roboshop-infra" }
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/frontend" },
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/catalogue" },
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/user" },
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/cart" },
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/shipping" },
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/payment" },
+    { name = "dispatch", folder = "CI-Pipeline", repo_url = "https://github.com/Bhanudevopss/dispatch" }
   ]
 }
 
